8576603a00a090aa696389e0dbf61019d1841e8f
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-22 13:16:00 +0000

    switched from jquery to angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,117 @@
-var socket = io();
+var app = angular.module('chatApp', ['btford.socket-io']);
 
-var chatWindow = document.getElementById('chatwindow');
-var name = '';
-
-socket.on('connect', function(){
-    console.log('Connected to socket.io server');
-});
-
-socket.on('message', function(message){
-    var momentTimestamp = moment.utc(message.timestamp);
-    console.log('New message');
-    console.log(message.content);
-
-    jQuery('.messages').append('<p><strong>' + message.name + ':</strong> ' + message.content + ' - ' + momentTimestamp.local().format('h:mm a') + '</p>');
-    chatWindow.scrollTop = chatWindow.scrollHeight;
-});
-
-// SET name
-var $nameForm = jQuery('#name-form');
-
-$nameForm.on('submit', function(event){
-    event.preventDefault();
-
-    var $username = $nameForm.find('input[name=username]');
-
-    if($username.val() !== ''){
-        console.log('Name: ' + $username.val());
-        name = $username.val();
-        $username.val('');
+app.directive('newMessage', function(){
+    return{
+        restrict: 'E',
+        template: '<p><strong>{{messageObj.name}}</strong>: {{messageObj.content}} <small>- {{messageObj.time()}}</small></p>',
+        scope: {
+            messageObj: '='
+        },
+        replace: true
     }
 });
 
-// SUBMIT NEW message
-var $form = jQuery('#message-form');
+app.factory('socket', ['socketFactory', function(socketFactory){
+    return socketFactory();
+}]);
 
-$form.on('submit', function(event){
-    event.preventDefault();
+app.controller('MainController', ['socket', '$scope', '$timeout', function(socket, $scope, $timeout){
 
-    var $message = $form.find('input[name=message]');
+    $scope.messages = [];
 
-    if($message.val() !== '' && name !== ''){
-        socket.emit('message', {
-            content: $message.val(),
-            name: name
+    var chatWindow = document.getElementById('chatwindow');
+
+    socket.on('connect', function(){
+        console.log('Connected to socket.io server');
+    });
+
+    socket.on('message', function(message){
+        var momentTimestamp = moment.utc(message.timestamp);
+        console.log('New message');
+        console.log(message.content);
+
+        message.time = function(){
+            var momentTimestamp = moment.utc(this.timestamp);
+            return momentTimestamp.local().format('h:mm a');
+        }
+
+        $scope.recievedMessage = message;
+
+
+        $scope.messages.push($scope.recievedMessage);
+
+        $timeout(function(){
+            chatWindow.scrollTop = chatWindow.scrollHeight;
         });
+    });
 
-        $message.val('');
+
+
+    $scope.myMessage = {
+        name: 'Cathal',
+        content: ''
     }
-});
+
+    $scope.sendMessage = function(){
+        if($scope.myMessage.content !== ''){
+            socket.emit('message', $scope.myMessage);
+
+            $scope.myMessage.content = '';
+        }
+    }
+
+
+}]);
+
+
+
+// var socket = io();
+//
+// var chatWindow = document.getElementById('chatwindow');
+// var name = '';
+//
+// socket.on('connect', function(){
+//     console.log('Connected to socket.io server');
+// });
+//
+// socket.on('message', function(message){
+//     var momentTimestamp = moment.utc(message.timestamp);
+//     console.log('New message');
+//     console.log(message.content);
+//
+//     jQuery('.messages').append('<p><strong>' + message.name + ':</strong> ' + message.content + ' - ' + momentTimestamp.local().format('h:mm a') + '</p>');
+//     chatWindow.scrollTop = chatWindow.scrollHeight;
+// });
+//
+// // SET name
+// var $nameForm = jQuery('#name-form');
+//
+// $nameForm.on('submit', function(event){
+//     event.preventDefault();
+//
+//     var $username = $nameForm.find('input[name=username]');
+//
+//     if($username.val() !== ''){
+//         console.log('Name: ' + $username.val());
+//         name = $username.val();
+//         $username.val('');
+//     }
+// });
+//
+// // SUBMIT NEW message
+// var $form = jQuery('#message-form');
+//
+// $form.on('submit', function(event){
+//     event.preventDefault();
+//
+//     var $message = $form.find('input[name=message]');
+//
+//     if($message.val() !== '' && name !== ''){
+//         socket.emit('message', {
+//             content: $message.val(),
+//             name: name
+//         });
+//
+//         $message.val('');
+//     }
+// });
